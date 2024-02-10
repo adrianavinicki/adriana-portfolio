fcb04e27834611569c10aa42b17feb948cfeab60
adrianavinicki <adriana.vinicki@gmail.com>
2024-02-10 11:36:36 -0300

    prueba contacto

---
+++ pages/api/send.js
@@ -0,0 +1,28 @@
+//import type { NextApiRequest, NextApiResponse } from "next";
+//import { EmailTemplate } from "../../components/EmailTemplate";
+import { Contact } from "../contact/index.js";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const correoPersonal = process.env.CORREO_PERSONAL;
+
+export default async (req, res) => {
+  if (req.method !== "POST")
+    return res.status(405).json({ error: "Method Not Allowed" });
+
+  const { name, email, subject, message } = req.body;
+
+  const { data, error } = await resend.emails.send({
+    from: "Acme <onboarding@resend.dev>",
+    to: [correoPersonal],
+    subject: "Mensaje de Posible Cliente",
+    react: Contact({ name, email, subject, message }),
+  });
+
+  if (error) {
+    return res.status(400).json(error);
+  }
+
+  return res.status(200).json(data);
+};
